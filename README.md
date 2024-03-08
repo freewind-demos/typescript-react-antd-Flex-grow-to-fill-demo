1aefd345f0347d382d8115d4c0332f61139572e6
Freewind <nowindlee@gmail.com>
2024-03-08 18:17:44 +0800

    fix flex:1

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ TypeScript React AntDesign Flex Grow to Fill Demo
 
 Flex组件默认就是block+flex，子元素会占满空间。
 
-还可以指定部分子元素的宽度，其它的会自动增长以占满剩余空间，比较方便
+如果指定部分子元素的宽度，希望其它子元素自动占满剩余空间，需要指定`flex:1`（即flex-grow: 1），不然各部分宽度不对
+
+如果Flex中有Flex，子Flex也需要指定`flex: 1`，否则不能占满。
+
+总之就是使用Flex组件并且在部分子元素上添加`flex:1`来控制
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -5,7 +5,14 @@ export default function Hello() {
   return <Flex vertical gap="small" style={{ width: 500, border: '2px solid red' }}>
     <Flex gap="small" align="baseline">
       <span style={{ width: 120, textAlign: 'right', border: '2px solid blue' }}>label</span>
-      <Input style={{ border: '2px solid green' }} />
+      <Input style={{ flex: 1, border: '2px solid green' }} />
+    </Flex>
+    <Flex gap="small" align="baseline">
+      <span style={{ width: 120, textAlign: 'right', border: '2px solid blue' }}>label</span>
+      <Flex vertical gap="small" style={{ flex: 1 }}>
+        <Input style={{ border: '2px solid green' }} />
+        <Input style={{ border: '2px solid green' }} />
+      </Flex>
     </Flex>
     <Button>Submit</Button>
   </Flex>
