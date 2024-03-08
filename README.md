b85b5e5a948abf17b26c19fb78eecd4ac68bd57d
Freewind <nowindlee@gmail.com>
2024-03-08 18:11:17 +0800

    typescript-react-antd-Flex-grow-to-fill-demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-TypeScript React AntDesign Demo
+TypeScript React AntDesign Flex Grow to Fill Demo
 =================================
+
+Flex组件默认就是block+flex，子元素会占满空间。
+
+还可以指定部分子元素的宽度，其它的会自动增长以占满剩余空间，比较方便
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
-import {Alert} from 'antd';
+import { Button, Flex, Input } from 'antd';
+import React from 'react';
 
 export default function Hello() {
-  return <Alert message={'Hello'}/>
+  return <Flex vertical gap="small" style={{ width: 500, border: '2px solid red' }}>
+    <Flex gap="small" align="baseline">
+      <span style={{ width: 120, textAlign: 'right', border: '2px solid blue' }}>label</span>
+      <Input style={{ border: '2px solid green' }} />
+    </Flex>
+    <Button>Submit</Button>
+  </Flex>
 };
